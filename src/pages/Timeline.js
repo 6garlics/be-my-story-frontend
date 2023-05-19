ee5e11bb63f081e8379546b5699150db171d7513
Jeongmin Cho <jojm0829@gmail.com>
2023-05-19 16:42:59 +0900

    ✨ feat: Timeline.js에서 postman 가짜서버 연결해서 books 데이터 받아옴

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,74 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import Book from "../components/Book";
-const books = [
-  {
-    userId: "Jamie",
-    profileImage: "images/Profile.jpg",
-    pages: [
-      {
-        image: "images/image1.png",
-        text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
-      },
-      {
-        image: "images/image2.png",
-        text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
-      },
-      {
-        image: "images/image3.png",
-        text: "“과자로 만들어진 집이네?”",
-      },
-      {
-        image: "images/image4.png",
-        text: "남매는 마녀에게 붙잡혀 버렸어요.",
-      },
-      {
-        image: "images/image5.png",
-        text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
-      },
-      {
-        image: "images/image6.png",
-        text: "형제들은 마침내 집을 완성했답니다.",
-      },
-    ],
-  },
-  {
-    userId: "Jamie",
-    profileImage: "images/Profile.jpg",
-    pages: [
-      {
-        image: "images/image1.png",
-        text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
-      },
-      {
-        image: "images/image2.png",
-        text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
-      },
-      {
-        image: "images/image3.png",
-        text: "“과자로 만들어진 집이네?”",
-      },
-      {
-        image: "images/image4.png",
-        text: "남매는 마녀에게 붙잡혀 버렸어요.",
-      },
-      {
-        image: "images/image5.png",
-        text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
-      },
-      {
-        image: "images/image6.png",
-        text: "형제들은 마침내 집을 완성했답니다.",
-      },
-    ],
-  },
-];
 
 function Timeline() {
+  const [books, setBooks] = useState([]);
+
+  const getBooks = async () => {
+    const response = await fetch(
+      "https://8d2f9c4b-049f-4bd4-81c4-e22ed6603982.mock.pstmn.io/books"
+    );
+    console.log(response);
+    const json = await response.json();
+    setBooks(json);
+    console.log(json);
+  };
+  useEffect(() => {
+    getBooks();
+  }, []);
+
   return (
     <Container>
-      {books.map((book) => (
-        <Book book={book} />
+      {books.map((book, index) => (
+        <Book key={index} book={book} />
       ))}
     </Container>
   );
