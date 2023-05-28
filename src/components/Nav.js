0baa0a303ccbd8ff01894b30a19ffb4c84518f9d
Jeongmin Cho <jojm0829@gmail.com>
2023-05-29 03:12:06 +0900

    💄 UI: Book, Page 컴포넌트 디자인 수정2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -13,39 +13,29 @@ import {
 const iconSize = 28;
 
 const Nav = () => {
-  const defaultStyle = {
-    color: "black",
-    textDecoration: "none",
-    fontSize: "18px",
-    margin: "15px 0px",
-    display: "flex",
-    alignItems: "center",
-  };
   const style = ({ isActive }) => {
-    return isActive
-      ? { ...defaultStyle, fontWeight: "bold" }
-      : { ...defaultStyle, color: "grey" };
+    return isActive ? { fontWeight: "bold" } : { color: "grey" };
   };
   return (
     <Container>
-      <NavLink to="/" style={style}>
+      <NLink to="/" style={style}>
         <Icon>
           <RiHome3Fill size={iconSize} />
         </Icon>
         타임라인
-      </NavLink>
-      <NavLink to="/diary-form" style={style}>
+      </NLink>
+      <NLink to="/diary-form" style={style}>
         <Icon>
           <RiAddCircleFill size={iconSize} />
         </Icon>
         일기쓰기
-      </NavLink>
-      <NavLink to="/bookshelf/0" style={style}>
+      </NLink>
+      <NLink to="/bookshelf/0" style={style}>
         <Icon>
           <RiBook2Fill size={iconSize} />
         </Icon>
         내 책장
-      </NavLink>
+      </NLink>
     </Container>
   );
 };
@@ -61,6 +51,21 @@ const Container = styled.div`
   padding-top: 30px;
   /* 햄버거 버튼으로 접었다 폈다 할 수 있게 하면 좋을 듯 */
   /* 화면 가로 길이 좁아지면 자동으로 접히게 하면 좋을 듯 */
+`;
+
+const NLink = styled(NavLink)`
+  display: flex;
+  align-items: center;
+  width: 140px;
+  height: 35px;
+  margin: 10px 0px;
+  border-radius: 20px 20px 20px 20px;
+  color: black;
+  text-decoration: none;
+  font-size: 18px;
+  &:hover {
+    //background: lightgrey;
+  }
 `;
 
 const Icon = styled.div`
