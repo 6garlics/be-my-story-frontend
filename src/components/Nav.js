c6eca6227c521c8f15c1da9df00c914c7f6ae3c9
Jeongmin Cho <jojm0829@gmail.com>
2023-05-29 03:00:28 +0900

    💄 UI: Nav 컴포넌트 디자인 수정

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,27 +1,49 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import { styled } from "styled-components";
+import {
+  RiHome3Fill,
+  RiHome3Line,
+  RiAddCircleFill,
+  RiAddCircleLine,
+  RiBook2Fill,
+  RiBook2Line,
+} from "react-icons/ri";
+
+const iconSize = 28;
 
 const Nav = () => {
   const defaultStyle = {
+    color: "black",
     textDecoration: "none",
-    fontSize: "20px",
-    margin: "5px 0px",
+    fontSize: "18px",
+    margin: "15px 0px",
+    display: "flex",
+    alignItems: "center",
   };
   const style = ({ isActive }) => {
     return isActive
-      ? { ...defaultStyle, color: "black", fontWeight: "bold" }
+      ? { ...defaultStyle, fontWeight: "bold" }
       : { ...defaultStyle, color: "grey" };
   };
   return (
     <Container>
       <NavLink to="/" style={style}>
+        <Icon>
+          <RiHome3Fill size={iconSize} />
+        </Icon>
         타임라인
       </NavLink>
       <NavLink to="/diary-form" style={style}>
+        <Icon>
+          <RiAddCircleFill size={iconSize} />
+        </Icon>
         일기쓰기
       </NavLink>
       <NavLink to="/bookshelf/0" style={style}>
+        <Icon>
+          <RiBook2Fill size={iconSize} />
+        </Icon>
         내 책장
       </NavLink>
     </Container>
@@ -32,13 +54,17 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   flex: none;
-  width: 100px;
+  width: 250px;
+  height: 100vh;
   height: auto;
   border-right: 1px solid grey;
   padding-top: 30px;
-  /* border-right가 화면 세로 길이만큼 되도록 수정해야 함. */
   /* 햄버거 버튼으로 접었다 폈다 할 수 있게 하면 좋을 듯 */
   /* 화면 가로 길이 좁아지면 자동으로 접히게 하면 좋을 듯 */
+`;
+
+const Icon = styled.div`
+  margin: 0px 12px -6px 13px;
 `;
 
 export default Nav;
