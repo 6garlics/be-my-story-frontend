d23a91b92b64da454e9a1dca9b9f170ae6775ecc
Jeongmin Cho <jojm0829@gmail.com>
2023-05-29 01:37:30 +0900

    💄 UI: 더미데이터 예쁘게 가공

--- src/data/BooksData.js
+++ src/data/BooksData.js
@@ -3,9 +3,10 @@ export const books = [
     {
       userId: 0,
       nickname: "Jamie",
-      profileImage: "/images/profile.jpg",
+      profileImage:
+        "https://t4.ftcdn.net/jpg/05/65/24/45/240_F_565244595_9DSsL5nS0nefC3wvjRLybz6UZt1JHvxM.jpg",
       id: 1,
-      title: "피터팬1",
+      title: "피터팬",
       pages: [
         {
           image: "/images/image1.png",
@@ -38,37 +39,37 @@ export const books = [
       nickname: "Jamie",
       profileImage: "/images/profile.jpg",
       id: 2,
-      title: "피터팬2",
+      title: "바닷가와 파라솔",
       pages: [
         {
           image:
             "https://t4.ftcdn.net/jpg/03/49/81/77/240_F_349817774_NvqiNi9fVYO1SIcdqiwaJGyjwT0dRI8e.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+          text: "바닷가 한가운데에 파라솔이 놓여져 있어요.",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/05/69/55/11/240_F_569551106_FELWntaV4NlNHL5Qjg5iCNMokor5Ujjb.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+          text: "우와! 엄청 커다란 유니콘이에요. 저걸 타고 달리면 엄청 빠르겠죠?",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/05/67/77/30/240_F_567773066_kew8LmnKsJKc8eYQbVC9vmJAGuXEQG2t.jpg",
-          text: "“과자로 만들어진 집이네?”",
+          text: "플라밍고도 수영을 하고 싶었나봐요. 튜브를 타고 둥둥 떠내려가요.",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/05/55/59/43/240_F_555594374_OXYC3gAnPNLfr3smkYsWPTQJVYPV3UYP.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+          text: "유리병 속에 캔디가 가득 들어 있어요. 저걸 꺼내서 먹을 수 있다면 얼마나 좋을까요?",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/05/74/60/11/240_F_574601145_HeTlovsyasJLam7oo68KcCqVxDGp6A6P.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
+          text: "달걀 두개가 깨져있어요. 무슨일이 벌어졌던 걸까요?",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/04/96/44/40/240_F_496444035_Y6r1Wgu0gVvben88exR8DYQLo4SgwNbd.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+          text: "오늘은 부활절! 달걀로 예쁘게 장식해보아요.",
         },
       ],
     },
@@ -77,37 +78,37 @@ export const books = [
       nickname: "Jamie",
       profileImage: "/images/profile.jpg",
       id: 3,
-      title: "피터팬3",
+      title: "할로윈데이",
       pages: [
         {
           image:
             "https://t3.ftcdn.net/jpg/05/37/60/32/240_F_537603256_DZAWtM86Z8LtQ0QDDAA3cN0jmRt0Idk7.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+          text: "오늘은 할로윈 데이에요. 잭오랜턴을 만들기 위해 호박을 잔뜩 사왔어요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/04/56/99/06/240_F_456990624_XjNK1xPQwQupyWatvJoT7dgBlHwQvbPI.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+          text: "이웃집을 돌아다니면서 온갖 사탕을 얻으러 다녀볼까요?",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/05/47/81/59/240_F_547815918_svDjWQ7kKO3BFO6SwVxiDUpZ9Ab9FZM2.jpg",
-          text: "“과자로 만들어진 집이네?”",
+          text: "가을의 한가운데에 서있어요. 여행을 떠날 준비를 해요.",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/05/35/05/35/240_F_535053513_CsI7EpQqdHC0ti0yWszlb7Hwcg0NFuXs.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+          text: "알록달록 낙엽이 마구 떨어져요. 떨어지는 낙엽을 잡아볼까요?",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/03/78/95/14/240_F_378951465_iraoDvnIpcA3FXjZ4nMITa9rHi7GqG8O.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
+          text: "엄마가 호박을 쪄주셨어요. 꿀을 발라서 먹으니 정말 꿀맛이에요!",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/04/61/70/77/240_F_461707725_6ZJArgLi4P1sySKALANjUtSgyEqUDhQ6.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+          text: "조금만 더 기다리면 크리스마스가 다가오겠죠?",
         },
       ],
     },
@@ -116,37 +117,37 @@ export const books = [
       nickname: "Jamie",
       profileImage: "/images/profile.jpg",
       id: 4,
-      title: "피터팬4",
+      title: "보라빛 물감",
       pages: [
         {
           image:
             "https://t3.ftcdn.net/jpg/04/68/13/10/240_F_468131014_MmBuETI8UgNSWx7DEfr3MS2N6EytAZGz.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+          text: "몽글몽글 어지러운 보라빛 물감이 막 섞이고 있어요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/05/64/99/18/240_F_564991851_fDtvcK35w0hMMRzgzgGuBd5ZAUctokSA.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+          text: "오늘은 하늘도 보라빛으로 물들어가네요. 너무나 아름다운 풍경이에요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/04/68/13/10/240_F_468131034_FM01kK10uxriTueCcSGiXw8FCrJ3r5Is.jpg",
-          text: "“과자로 만들어진 집이네?”",
+          text: "반짝반짝 빛나는 보라빛 풍경 속에 빠져보아요.",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/05/25/41/11/240_F_525411143_dW6gF1VBCSF98ZRynHXBSEOorXYr3nz1.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+          text: "보라빛 하늘 틈새로 어느새 눈송이가 내리기 시작했어요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/05/63/24/98/240_F_563249886_fZ4EHbd4nb2vX3RfF62uSNrBG1oJb9LA.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
+          text: "우와! 예쁜 보라빛 하트 장식이네요. 누가 만들었을까요?",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/05/51/55/00/240_F_551550036_QciedZlban6rQARwpPbdRIcxC84GM8KN.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+          text: "저 멀리 보라빛 하늘 속에서 해가 뉘엿뉘엿 져가고 있어요.",
         },
       ],
     },
@@ -155,37 +156,37 @@ export const books = [
       nickname: "Jamie",
       profileImage: "/images/profile.jpg",
       id: 5,
-      title: "피터팬5",
+      title: "숲속에서",
       pages: [
         {
           image:
             "https://t4.ftcdn.net/jpg/05/92/82/95/240_F_592829505_IG9e7tkavJR8bubPV5W3PXtZ6Wn15uFm.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+          text: "푸른 숲속에 무대가 놓여져 있어요. 어떤 공연이 열리는 걸까요?",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/03/75/42/90/240_F_375429028_bfErdWY3UOa53WmjMlOg7mk6vPce77XZ.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+          text: "소나무 밑에 파뭍혔어요! 도무지 헤어나올 수가 없네요.",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/02/63/51/81/240_F_263518160_FmQVnE364go0ueSd2weWEcn4rYM8odat.jpg",
-          text: "“과자로 만들어진 집이네?”",
+          text: "맛있는 청사과를 야무지게 잘라서 먹어보아요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/05/28/08/40/240_F_528084080_FOmtMLgSJvMpusOAKWY3dsHJSFPgrMtZ.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+          text: "역시 여름에는 수박이죠. 수박도 잘라서 먹어보아요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/05/92/82/94/240_F_592829469_bc9AVJytJSY4ae53o4ha4Ph79pdRvqSo.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
+          text: "숲속에 또 다른 무대가 있네요. 오늘은 축제가 열리려나 봐요.",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/00/09/13/83/240_F_9138319_qkVjvBaSFvx4z1VDb8rqkDT5zmDY1rRi.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+          text: "햇빛에 투명하게 빛나는 나뭇잎을 바라보아요.",
         },
       ],
     },
@@ -194,37 +195,37 @@ export const books = [
       nickname: "Jamie",
       profileImage: "/images/profile.jpg",
       id: 6,
-      title: "피터팬6",
+      title: "구름과 하늘",
       pages: [
         {
           image:
             "https://t4.ftcdn.net/jpg/00/57/93/51/240_F_57935120_XFL0EdJJq5fPXoxsc9O8uwMQKzkEfh42.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+          text: "구름이 몽글몽글 떠있는 예쁜 하늘이이에요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/00/92/21/66/240_F_92216615_bLySNjZrHZGBvf01vwTEBr5kYn9b2T3T.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+          text: "밤하늘에는 은하수가 둥둥 떠있군요?",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/02/30/85/64/240_F_230856416_HzaH5EV2PJKKqLnKVnIWpqpJjUwxFIIM.jpg",
-          text: "“과자로 만들어진 집이네?”",
+          text: "해가 질 무렵에는 하늘이 핑크빛으로 물들어가요.",
         },
         {
           image:
             "https://t3.ftcdn.net/jpg/00/44/18/02/240_F_44180204_uFakOlqnp9lyt8ipij19Tz5DXNuX0kpl.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+          text: "다음날이 되어도 여전히 하늘에는 구름이 몽실몽실 떠있어요.",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/00/70/39/57/240_F_70395754_JG0OjMP1C47rkE0jjdroRxlB5SqDSqLc.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
+          text: "밤이되자 밤하늘에는 별들이 빼곡하게 빛나고 있어요",
         },
         {
           image:
             "https://t4.ftcdn.net/jpg/00/85/10/25/240_F_85102503_CJDKLzmrZKDHmKyZSRVstnHbZfUPaYrc.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+          text: "오늘은 어제보다 구름이 더 많이 떠있네요!",
         },
       ],
     },
@@ -232,40 +233,42 @@ export const books = [
   [
     {
       userId: 1,
-      nickname: "Emma1",
-      profileImage: "/images/profile.jpg",
+      nickname: "Emma",
+      profileImage:
+        "https://t4.ftcdn.net/jpg/05/86/51/75/240_F_586517584_wP9bk2PFqpcADyEVMWxtDEvTrrtLc8sD.jpg",
       id: 11,
       title: "피터팬11",
       pages: [
         {
           image:
-            "https://t3.ftcdn.net/jpg/04/75/58/76/240_F_475587626_eLV7nd1effOBqiQ0laEodweWsrlpZ1b6.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+            "https://t3.ftcdn.net/jpg/02/64/87/94/240_F_264879427_VlgCm1zDoDSlpou3ZzpPXa0acm8xp0HB.jpg",
+          text: "모래사장에 파라솔과 의자 두 개가 놓여있어요.",
         },
         {
           image:
-            "https://t4.ftcdn.net/jpg/05/60/54/05/240_F_560540507_UidiMKRN01YdO5Sfy9L2fAgg36IzQZnl.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+            "https://t3.ftcdn.net/jpg/04/85/77/78/240_F_485777832_WkqOHv2yYIR0RjgDFsZSkQHgbD2cl3yq.jpg",
+          text: "상큼한 오렌지가 정말 많이 있네요! 목이 마르니까 하나를 집어 먹어볼까요?",
+        },
+
+        {
+          image:
+            "https://t4.ftcdn.net/jpg/02/02/47/03/240_F_202470360_LIaoGGah9UbZ4xUGeY1uUAuCxxSHeRiq.jpg",
+          text: "오렌지보다 더 상큼한 레몬이에요. 먹으면 인상이 찌푸려질지도 몰라요.",
         },
         {
           image:
-            "https://t4.ftcdn.net/jpg/00/00/55/65/240_F_556530_rvbZfl530SrbYItvtzjhIGrk4FLau0.jpg",
-          text: "“과자로 만들어진 집이네?”",
+            "https://t4.ftcdn.net/jpg/01/02/25/43/240_F_102254383_oyiqTp42e5lbY8dJLZkuybvshv15JWGx.jpg",
+          text: "핑크빛 하늘을 바라봐요. 오늘은 하늘색이 참 예쁘네요.",
         },
         {
           image:
-            "https://t4.ftcdn.net/jpg/02/74/61/59/240_F_274615900_nQOlPBwRHNW17TMeWnhgsxqrujcRDrdq.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+            "https://t3.ftcdn.net/jpg/02/77/07/80/240_F_277078013_Gd3miIHP0hyzwJC7xEVgzUNFEt6KpiWw.jpg",
+          text: "정말 예쁜 식물이에요. 물을 먹고 무럭무럭 자라나요.",
         },
         {
           image:
-            "https://t3.ftcdn.net/jpg/00/72/51/42/240_F_72514274_iRNif8oLy6rMKuAtwxDYf3ZvURNCC4Fk.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
-        },
-        {
-          image:
-            "https://t4.ftcdn.net/jpg/01/47/46/27/240_F_147462743_uDtbbazYpHyABajHURMipTq64exOLxZ5.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+            "https://t3.ftcdn.net/jpg/00/42/31/24/240_F_42312433_hGQdTNqiOAD0yZOHBJU2qUbrdhvgdMjB.jpg",
+          text: "더운 여름에는 시원한 밀크쉐이크를 마셔보아요.",
         },
       ],
     },
@@ -273,40 +276,41 @@ export const books = [
   [
     {
       userId: 2,
-      nickname: "Emma2",
-      profileImage: "/images/profile.jpg",
+      nickname: "Dale",
+      profileImage:
+        "https://t3.ftcdn.net/jpg/03/51/27/12/240_F_351271275_wyPlSeAf1vtgzK2rWucYof3RNxXUpcqb.jpg",
       id: 22,
       title: "피터팬22",
       pages: [
         {
           image:
-            "https://t4.ftcdn.net/jpg/00/52/46/49/240_F_52464981_MCSxpqNBMkV7z4Da5GxlEfr4fjjGb5zn.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+            "https://t3.ftcdn.net/jpg/02/72/50/22/240_F_272502277_RmxMFEPMbVXkSvbZqVMYec7M07fWyLkF.jpg",
+          text: "해변가에 누워 햇빛을 즐겨보아요.",
         },
         {
           image:
-            "https://t4.ftcdn.net/jpg/00/74/49/23/240_F_74492335_deJlFi3DlD8Z2OPhjLb6RrnnCyJyhAxB.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+            "https://t4.ftcdn.net/jpg/05/88/21/89/240_F_588218935_O6MdpuTzReikgyS3LQYNEQz6TPQuEasz.jpg",
+          text: "이런! 아이스크림이 바닥에 엎어져서 흘러내리고 있네요.",
         },
         {
           image:
-            "https://t4.ftcdn.net/jpg/05/91/51/69/240_F_591516922_vzcJLKM5f0KQ0ss0QIHPgJhALIDPKQSR.jpg",
-          text: "“과자로 만들어진 집이네?”",
+            "https://t4.ftcdn.net/jpg/04/24/76/79/240_F_424767910_J8dAh14hZfzw0ac2xCwcpS9iJCvtMheW.jpg",
+          text: "수영장이 노을을 받아 오묘한 색으로 빛나고 있어요.",
         },
         {
           image:
-            "https://t4.ftcdn.net/jpg/03/76/78/85/240_F_376788519_wH17GhWSVJISV47gJYSruNxP3VvTDORz.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+            "https://t4.ftcdn.net/jpg/05/99/24/49/240_F_599244998_czCcCR7wHny8hq0hzbTqIreS7E1X6eVH.jpg",
+          text: "뜨거운 햇살에 타지 않으려면 선크림을 꼭 발라야겠죠?",
         },
         {
           image:
-            "https://t3.ftcdn.net/jpg/00/64/93/42/240_F_64934225_M04oBPilIHzTfvT8sUFqCZZUlSHWL2wX.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
+            "https://t4.ftcdn.net/jpg/02/59/47/75/240_F_259477564_8fT1fLaeJeuArOYgTU94JJ2vtOLFzHiy.jpg",
+          text: "해수욕장은 벌써 사람들로 북적이고 있네요!",
         },
         {
           image:
-            "https://t3.ftcdn.net/jpg/01/64/80/40/240_F_164804037_OS8mfqyqPiipvb0OLVXeXyI2XUjldmjs.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+            "https://t4.ftcdn.net/jpg/01/67/60/61/240_F_167606116_kC0MXp6nv7hKkeOYYRn3DB3hhkCx6f5g.jpg",
+          text: "챙겨온 튜브와 공을 가지고 신나게 놀아볼까요?",
         },
       ],
     },
@@ -314,40 +318,41 @@ export const books = [
   [
     {
       userId: 3,
-      nickname: "Emma3",
-      profileImage: "/images/profile.jpg",
+      nickname: "Laila",
+      profileImage:
+        "https://t4.ftcdn.net/jpg/05/39/41/45/240_F_539414501_6hVkw2eyA3mTAFgwvVPWNBaAvIKTzl02.jpg",
       id: 33,
       title: "피터팬33",
       pages: [
         {
           image:
-            "https://t3.ftcdn.net/jpg/04/93/43/84/240_F_493438432_bru7DujXhwgEOTN7Dgp14kJTLBYldZjg.jpg",
-          text: "피터팬과 아이들은 함께 하늘로 날아 올랐어요.",
+            "https://t3.ftcdn.net/jpg/01/89/74/52/240_F_189745203_9m9RUTCRGIVasizdoOszGqIv6o1W9PD8.jpg",
+          text: "꽃과 함께 선물 포장을 해보아요. 받는 사람의 기분이 참 좋아질 것 같아요.",
         },
         {
           image:
-            "https://t3.ftcdn.net/jpg/00/70/77/26/240_F_70772623_oUcdNsnXgdhOoiuyUDMG6joxsqlBHmsv.jpg",
-          text: "피터팬은 후크선장을 악어에게 데리고 갔어요.",
+            "https://t3.ftcdn.net/jpg/05/71/08/08/240_F_571080805_X0QAWP59snXnZt0HkMcLHGXhfLM5Mh6c.jpg",
+          text: "저 분홍색 상자 안에는 무엇이 들어있을까요?",
         },
         {
           image:
-            "https://t3.ftcdn.net/jpg/00/98/15/40/240_F_98154034_wrLlJJ4XWoIoT4HZff1RRTikoaiGxQGk.jpg",
-          text: "“과자로 만들어진 집이네?”",
+            "https://t3.ftcdn.net/jpg/03/39/30/62/240_F_339306270_Q4QkvdBxonPcch85zHeCpTmvBIkaLwVc.jpg",
+          text: "어머니의 날이에요. 꽃과 함께 마음을 담은 편지를 드렸어요.",
         },
         {
           image:
-            "https://t3.ftcdn.net/jpg/03/05/90/06/240_F_305900613_5k250lNXucOTRhfQsje5OkX1xoOtCvK7.jpg",
-          text: "남매는 마녀에게 붙잡혀 버렸어요.",
+            "https://t3.ftcdn.net/jpg/01/93/55/40/240_F_193554081_Hhs5NiNtcGuCIlV6yCcEg5UurVALrUKy.jpg",
+          text: "나뭇잎들도 꽃 못지않게 예쁘네요.",
         },
         {
           image:
-            "https://t4.ftcdn.net/jpg/00/34/41/31/240_F_34413186_ZobpTGkVpI3AZ5Qc5beN6sqh4gJZhus6.jpg",
-          text: "아기 돼지 삼형제는 집을 지으러 떠났어요.",
+            "https://t3.ftcdn.net/jpg/03/01/55/10/240_F_301551029_NPc9wxpn5IxtfOw0Z5ag9fvgF5mqY58l.jpg",
+          text: "노란 배경과 하얀 꽃들이에요. 왠지 달콤한 향기가 날 것 같아요.",
         },
         {
           image:
-            "https://t3.ftcdn.net/jpg/01/02/50/92/240_F_102509211_3fHBaM2TwVqXL4lJYVwrOzOmPz9vR4IK.jpg",
-          text: "형제들은 마침내 집을 완성했답니다.",
+            "https://t3.ftcdn.net/jpg/05/71/08/08/240_F_571080829_HPXQUu54328Z038zxyWfCEZ4xL7QHl39.jpg",
+          text: "봄에는 분홍색 벚꽃이 아름답게 피어나요. 함께 벚꽃 구경을 가볼까요?",
         },
       ],
     },
--- src/data/UsersData.js
+++ src/data/UsersData.js
@@ -3,32 +3,32 @@ import { books } from "./BooksData";
 export const users = [
   {
     userId: 0,
-    nickname: "Jamie",
-    profileImage: "/images/profile.jpg",
+    nickname: books[0][0].nickname,
+    profileImage: books[0][0].profileImage,
     myBooks: books[0],
     myFriends: [...books[1], ...books[2], ...books[3]],
   },
 
   {
     userId: 1,
-    nickname: "Emma1",
-    profileImage: "/images/profile.jpg",
+    nickname: books[1][0].nickname,
+    profileImage: books[1][0].profileImage,
     myBooks: books[1],
     myFriends: [...books[2], ...books[3]],
   },
 
   {
     userId: 2,
-    nickname: "Emma2",
-    profileImage: "/images/profile.jpg",
+    nickname: books[2][0].nickname,
+    profileImage: books[2][0].profileImage,
     myBooks: books[2],
     myFriends: [...books[1], ...books[3]],
   },
 
   {
     userId: 3,
-    nickname: "Emma3",
-    profileImage: "/images/profile.jpg",
+    nickname: books[3][0].nickname,
+    profileImage: books[3][0].profileImage,
     myBooks: books[3],
     myFriends: [...books[1], ...books[2]],
   },
