5b907fb5b041dcbaf0149026b5a7e5a6aacf8dea
Jeongmin Cho <jojm0829@gmail.com>
2023-07-24 00:22:09 +0900

    💄 Design: 모달 디자인 완성 (아직 좀 구림)

--- src/components/timeline/Modal.js
+++ src/components/timeline/Modal.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import { styled } from "styled-components";
+import { IoClose } from "react-icons/io5";
 
 const Modal = ({ open, setOpen }) => {
   useEffect(() => {
@@ -28,7 +29,25 @@ const Modal = ({ open, setOpen }) => {
           setOpen((prev) => !prev);
         }}
       ></Layer>
-      <Container>모달</Container>
+      <Container>
+        <Inner>
+          <CloseBtn onClick={() => setOpen((prev) => !prev)}>
+            <IoClose size={22} color="#78b9ff" />
+          </CloseBtn>
+          <Header>
+            <Date>2023년 7월 23일 일요일</Date>
+            <Genre>모험</Genre>
+          </Header>
+
+          <Title>제목: 자전거</Title>
+
+          <Contents>
+            오늘 밤에 자전거를 탔다. 자전거는 처음 탈 때는 좀 중심잡기가
+            힘들었다. 그러나 재미있었다. 자전거를 잘 타서 엄마, 아빠 산책 갈 때
+            나도 가야겠다.
+          </Contents>
+        </Inner>
+      </Container>
     </>
   );
 };
@@ -48,9 +67,28 @@ const Layer = styled.div`
 
 const Container = styled.div`
   z-index: 2;
+  width: 450px;
+  height: 650px;
+  /* background: black; */
+  background: #78b9ff;
+  box-sizing: border-box;
+  border-radius: 20px;
+  box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.15);
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Inner = styled.div`
+  /* z-index: 2; */
   width: 400px;
-  height: 248px;
-  border-radius: 9px;
+  height: 600px;
+  padding: 45px 20px;
+  box-sizing: border-box;
+  /* border-radius: 9px; */
+  /* outline: 17px solid #78b9ff; */
   background: #fff;
   box-shadow: 0px 0px 18px 0px rgba(0, 0, 0, 0.15);
   display: flex;
@@ -60,4 +98,54 @@ const Container = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+`;
+
+const CloseBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 45px;
+  width: 45px;
+  margin: 0;
+  padding: 7px;
+  position: absolute;
+  top: 0;
+  right: 0;
+  background: none;
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
+  /* border: 1px solid red; */
+`;
+
+const Header = styled.div`
+  display: flex;
+  padding: 10px 5px;
+  border-top: 2px solid #78b9ff;
+`;
+
+const Date = styled.div``;
+
+const Genre = styled.div`
+  margin-left: auto;
+  font-size: 12px;
+  font-weight: bold;
+  background: #78b9ff;
+  color: white;
+  padding: 0 7px;
+  padding-top: 2px;
+  text-align: center;
+  border-radius: 20px;
+`;
+
+const Title = styled.div`
+  padding: 20px 5px;
+  border-top: 2px solid #78b9ff;
+`;
+
+const Contents = styled.div`
+  padding: 0px 5px;
+  flex: auto;
+  border-bottom: 2px solid #78b9ff;
 `;
