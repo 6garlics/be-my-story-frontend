9c23b6bf6a159c039e0cbfd2b8b82889d2460a2c
Jeongmin Cho <jojm0829@gmail.com>
2023-08-07 16:40:24 +0900

    Page에 props 설정

--- src/components/common/Page.js
+++ src/components/common/Page.js
@@ -1,17 +1,27 @@
 import React from "react";
 import { styled } from "styled-components";
 
-const Page = ({ imgUrl }) => {
+const Page = ({
+  imgUrl = "/images/dummy3.png",
+  text = "시작시작이것은 동화 이것은 동화 이것은 동화 이것은 동화이것은 동화이것은 동화 이것은 동화 이것은 동화 이것은 동화 이것은 동화이것은 동화이것은 동화 이것은 동화 이것은 동화 이것은 동화 이것은 동화이것은 동화이것은 동화 이것은 동화 이것은 동화 이것은 동화 이것은 동화 끝끝끝",
+  pageNum = 1,
+  onLeftClick,
+  onRightClick,
+}) => {
   return (
     <Container>
       <ImageWrapper>
-        <Image src="/images/dummy3.png" />
+        <Image src={imgUrl} />
+        <Button onClick={onLeftClick} $left="0px" $right="auto">
+          왼쪽
+        </Button>
       </ImageWrapper>
       <TextWrapper>
-        <Text>
-          이것은 동화 이것은 동화 이것은 동화 이것은 동화이것은 동화이것은 동화
-          이것은 동화 이것은 동화 이것은 동화 이것은 동화이것은 동화이것은 동화
-        </Text>
+        <Text>{text}</Text>
+        <PageNum>{pageNum}</PageNum>
+        <Button onClick={onRightClick} $left="auto" $right="0px">
+          오른쪽
+        </Button>
       </TextWrapper>
     </Container>
   );
@@ -23,17 +33,22 @@ const Container = styled.div`
   display: flex;
   border: 1px solid red;
   width: 90%;
+  min-width: 500px;
   border-radius: 10px;
   overflow: hidden;
 `;
 
 const ImageWrapper = styled.div`
   width: 50%;
-  border: 1px solid blue;
+  padding-bottom: 50%;
+  height: 0;
+  border: 3px solid blue;
+  position: relative;
 `;
 
 const Image = styled.img`
   width: 100%;
+  position: absolute;
 `;
 
 const TextWrapper = styled.div`
@@ -43,15 +58,38 @@ const TextWrapper = styled.div`
   justify-content: center;
   align-items: center;
   border: 1px solid yellow;
-  padding: 30px;
   box-sizing: border-box;
   background: skyblue;
+  color: black;
+  position: relative;
 `;
 
 const Text = styled.div`
   border: 1px solid white;
   white-space: normal;
-  color: black;
   font-size: 18px;
   line-height: 30px;
+  position: absolute;
+  padding: 30px;
+  box-sizing: border-box;
+  max-height: 100%;
+  border: 3px solid red;
+`;
+
+const PageNum = styled.div`
+  position: absolute;
+  right: 20px;
+  bottom: 20px;
+`;
+
+const Button = styled.button`
+  position: absolute;
+  width: 70%;
+  height: 100%;
+  left: ${(props) => props.$left};
+  right: ${(props) => props.$right};
+  opacity: 0;
+  &:hover {
+    cursor: pointer;
+  }
 `;
