3eda443cd5d5e9e70ccea4b233b1c1e06272af33
Jeongmin Cho <jojm0829@gmail.com>
2023-10-22 00:14:59 +0900

    AI 서버 주소 뒤의 슬래쉬 삭제

--- src/api/AIclient.js
+++ src/api/AIclient.js
@@ -4,7 +4,7 @@ const AIclient = axios.create();
 // AIclient.defaults.baseURL = "http://220.117.120.143:8000";
 // AIclient.defaults.withCredentials = true;
 AIclient.defaults.baseURL =
-  "https://orcaterm.tencentcloud.com/ide/05d1513b-6a0d-4f91-934b-5a0816803c82/proxy/8000/";
+  "https://orcaterm.tencentcloud.com/ide/05d1513b-6a0d-4f91-934b-5a0816803c82/proxy/8000";
 AIclient.defaults.headers.common["Content-Type"] = "application/json";
 
 export default AIclient;
