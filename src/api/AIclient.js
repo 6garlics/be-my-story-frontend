1cae7c0fd41f2ba043785aa9511c7c3d8f8ce0c5
Jeongmin Cho <jojm0829@gmail.com>
2023-10-22 12:57:08 +0900

    AI 서버 텐센트 클라우드로 연결

--- src/api/AIclient.js
+++ src/api/AIclient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const AIclient = axios.create();
-AIclient.defaults.baseURL = "http://220.117.120.143:8000";
+// AIclient.defaults.baseURL = "http://220.117.120.143:8000";
+AIclient.defaults.baseURL = "http://tori-fairytale.store";
 // AIclient.defaults.withCredentials = true;
 AIclient.defaults.headers.common["Content-Type"] = "application/json";
 
