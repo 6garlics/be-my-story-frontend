86d3634f767665e45830db98ae3aa16495006e50
Jeongmin Cho <jojm0829@gmail.com>
2023-10-22 14:57:37 +0900

    AI 서버를 https로

--- src/api/AIclient.js
+++ src/api/AIclient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AIclient = axios.create();
 // AIclient.defaults.baseURL = "http://220.117.120.143:8000";
-AIclient.defaults.baseURL = "http://tori-fairytale.store";
+AIclient.defaults.baseURL = "https://tori-fairytale.store";
 // AIclient.defaults.withCredentials = true;
 AIclient.defaults.headers.common["Content-Type"] = "application/json";
 
